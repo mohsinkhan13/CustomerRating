921cf50d0d836828ebcead067e345fd5cc5a098d
Janko.Sabados <Janko.Sabados@wolterskluwer.com>
2017-04-26 12:02:15 +0100

    Added WebSite MVC project and page for Customer Ratings.

---
+++ CodeGames2017.CustomerRatings/CodeGames2017.CustomerRating.WebSite/Models/RatingViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeGames2017.CustomerRating.WebSite.Models
+{
+    public class RatingViewModel
+    {
+        public Guid RatingId { get; set; }
+        public string Application { get; set; }
+        public string Section { get; set; }
+        public int RatingValue { get; set; }
+        public string Comment { get; set; }
+        public string RatedBy { get; set; }
+        public string ClientCode { get; set; }
+    }
+}
